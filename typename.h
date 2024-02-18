df08dfaffb9af85156241e1bd85ca004e1f48f34
shinnyeonki <sygys10293@gmail.com>
2024-02-18 19:00:13 +0900

    init

---
+++ typename.h
@@ -0,0 +1,56 @@
+#define typename(x) _Generic((x),                                                                                         \
+              _Bool: "_Bool",                                  unsigned char: "unsigned char",                             \
+               char: "char",                                     signed char: "signed char",                               \
+          short int: "short int",                         unsigned short int: "unsigned short int",                        \
+                int: "int",                                     unsigned int: "unsigned int",                              \
+           long int: "long int",                           unsigned long int: "unsigned long int",                         \
+      long long int: "long long int",                 unsigned long long int: "unsigned long long int",                    \
+              float: "float",                                         double: "double",                                    \
+        long double: "long double",                                                                                        \
+                                                                                                                           \
+            _Bool *: "_Bool *",                              unsigned char *: "unsigned char *",                           \
+             char *: "char *",                                 signed char *: "signed char *",                             \
+        short int *: "short int *",                     unsigned short int *: "unsigned short int *",                      \
+              int *: "int *",                                 unsigned int *: "unsigned int *",                            \
+         long int *: "long int *",                       unsigned long int *: "unsigned long int *",                       \
+    long long int *: "long long int *",             unsigned long long int *: "unsigned long long int *",                  \
+            float *: "float *",                                     double *: "double *",                                  \
+      long double *: "long double *",                                                                                      \
+                                                                                                                           \
+           _Bool **: "_Bool **",                            unsigned char **: "unsigned char **",                          \
+            char **: "char **",                               signed char **: "signed char **",                            \
+       short int **: "short int **",                   unsigned short int **: "unsigned short int **",                     \
+             int **: "int **",                               unsigned int **: "unsigned int **",                           \
+        long int **: "long int **",                     unsigned long int **: "unsigned long int **",                      \
+   long long int **: "long long int **",           unsigned long long int **: "unsigned long long int **",                 \
+           float **: "float **",                                   double **: "double **",                                 \
+     long double **: "long double **",                                                                                     \
+                                                                                                                           \
+          _Bool ***: "_Bool ***",                          unsigned char ***: "unsigned char ***",                         \
+           char ***: "char ***",                             signed char ***: "signed char ***",                           \
+      short int ***: "short int ***",                 unsigned short int ***: "unsigned short int ***",                    \
+            int ***: "int ***",                             unsigned int ***: "unsigned int ***",                          \
+       long int ***: "long int ***",                   unsigned long int ***: "unsigned long int ***",                     \
+  long long int ***: "long long int ***",         unsigned long long int ***: "unsigned long long int ***",                \
+          float ***: "float ***",                                 double ***: "double ***",                                \
+    long double ***: "long double ***",                                                                                    \
+                                                                                                                           \
+         _Bool ****: "_Bool ****",                        unsigned char ****: "unsigned char ****",                        \
+          char ****: "char ****",                           signed char ****: "signed char ****",                          \
+     short int ****: "short int ****",               unsigned short int ****: "unsigned short int ****",                   \
+           int ****: "int ****",                           unsigned int ****: "unsigned int ****",                         \
+      long int ****: "long int ****",                 unsigned long int ****: "unsigned long int ****",                    \
+ long long int ****: "long long int ****",       unsigned long long int ****: "unsigned long long int ****",               \
+         float ****: "float ****",                               double ****: "double ****",                               \
+   long double ****: "long double ****",                                                                                   \
+                                                                                                                           \
+        _Bool *****: "_Bool *****",                      unsigned char *****: "unsigned char *****",                       \
+         char *****: "char *****",                         signed char *****: "signed char *****",                         \
+    short int *****: "short int *****",             unsigned short int *****: "unsigned short int *****",                  \
+          int *****: "int *****",                         unsigned int *****: "unsigned int *****",                        \
+     long int *****: "long int *****",               unsigned long int *****: "unsigned long int *****",                   \
+long long int *****: "long long int *****",     unsigned long long int *****: "unsigned long long int *****",              \
+        float *****: "float *****",                             double *****: "double *****",                              \
+  long double *****: "long double *****",                                                                                  \
+                                                                                                                           \
+            default: "not supported type")
